81cccc94903427f34626170977c4fa1cf154a8ff
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2018-04-13 20:39:57 -0300

    Added message struct and initial subscribers

---
+++ subscriber.go
@@ -0,0 +1,68 @@
+package hub
+
+import (
+	gendiodes "code.cloudfoundry.org/go-diodes"
+)
+
+type (
+	// nonBlockingSubscriber uses an diode and is optiomal for many writes and a single reader
+	// This subscriber is used when need high throughput and losing data is acceptable.
+	nonBlockingSubscriber struct {
+		d *gendiodes.Poller
+	}
+
+	// blockingSubscriber uses an channel to receive events.
+	blockingSubscriber struct {
+		ch chan Message
+	}
+
+	discardSubscriber int
+)
+
+// NewNonBlockingSubscriber returns a new NonBlockingSubscriber diode to be used
+// with many writers and a single reader.
+func NewNonBlockingSubscriber(cap int, alerter gendiodes.Alerter) Subscriber {
+	return &nonBlockingSubscriber{
+		d: gendiodes.NewPoller(
+			gendiodes.NewManyToOne(cap, alerter)),
+	}
+}
+
+// Set inserts the given Event into the diode.
+func (d *nonBlockingSubscriber) Set(data Message) {
+	d.d.Set(gendiodes.GenericDataType(&data))
+}
+
+// Next will return the next Event. If the
+// diode is empty this method will block until a Event is available to be
+// read or context is done. In case of context done we will return true on the second return param.
+func (d *nonBlockingSubscriber) Next() (Message, bool) {
+	data := d.d.Next()
+	if data == nil {
+		return Message{}, true
+	}
+	return *(*Message)(data), false
+}
+
+// NewBlockingSubscriber returns a new blocking subscriber using chanels imternally.
+func NewBlockingSubscriber(cap int) Subscriber {
+	return &blockingSubscriber{
+		ch: make(chan Message, cap),
+	}
+}
+
+// Set inserts the given Event into the diode.
+func (s *blockingSubscriber) Set(msg Message) {
+	s.ch <- msg
+}
+
+// Next will return the next Event. If the
+// diode is empty this method will block until a Event is available to be
+// read or context is done. In case of context done we will return true on the second return param.
+func (s *blockingSubscriber) Next() (Message, bool) {
+	msg, ok := <-s.ch
+	return msg, ok
+}
+
+func (d discardSubscriber) Set(msg Message)       {}
+func (d discardSubscriber) Next() (Message, bool) { return Message{}, false }
