c194a16818adebfc75cdba9f99092f7cc7426af5
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2018-04-24 00:04:48 -0300

    Remove message helper functions
    it's hard to do the right thing when casting errors happen.
    Sometimes we need to return the zero value, others panic or log
    To avoid problems this cast must be done on the subscription side

--- message.go
+++ message.go
@@ -1,19 +1,14 @@
 package hub
 
-import (
-	"log"
-	"time"
-)
-
 type (
-	// Fields is a [key]value storage for Messages.
+	// Fields is a [key]value storage for Messages values.
 	Fields map[string]interface{}
 
 	// Message represent some message/event passed into the hub
 	// It also contain some helper functions to convert the fields to primitive types.
 	Message struct {
 		Name   string
-		Msg    []byte
+		Body   []byte
 		Fields Fields
 	}
 )
@@ -21,58 +16,4 @@ type (
 // Topic return the message topic used when the message was sended.
 func (m *Message) Topic() string {
 	return m.Name
-}
-
-// Int return an int field of an Message.
-func (m *Message) Int(key string) int {
-	v, ok := m.Fields[key].(int)
-	if !ok {
-		log.Fatalf("Message %#v didn`t have the int field %s", m, key)
-	}
-	return v
-}
-
-// Int64 return an int64 field of an Message.
-func (m *Message) Int64(key string) int64 {
-	v, ok := m.Fields[key].(int64)
-	if !ok {
-		log.Fatalf("Message %#v didn`t have the int64 field %s", m, key)
-	}
-	return v
-}
-
-// Int32 return an int32 field of an Message.
-func (m *Message) Int32(key string) int32 {
-	v, ok := m.Fields[key].(int32)
-	if !ok {
-		log.Fatalf("Message %#v didn`t have the int32 field %s", m, key)
-	}
-	return v
-}
-
-// Float64 return a float64 field of an Message.
-func (m *Message) Float64(key string) float64 {
-	v, ok := m.Fields[key].(float64)
-	if !ok {
-		log.Fatalf("Message %#v didn`t have the float64 field %s", m, key)
-	}
-	return v
-}
-
-// String return a string field of an Message.
-func (m *Message) String(key string) string {
-	v, ok := m.Fields[key].(string)
-	if !ok {
-		log.Fatalf("Message %#v didn`t have the string field %s", m, key)
-	}
-	return v
-}
-
-// Duration return a time.Duration field of an Message.
-func (m *Message) Duration(key string) time.Duration {
-	v, ok := m.Fields[key].(time.Duration)
-	if !ok {
-		log.Fatalf("Message %#v didn`t have the time.Duration field %s", m, key)
-	}
-	return v
 }
