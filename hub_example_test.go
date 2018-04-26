99892f920e6a43d779acb3393beb5eeb46b5a89e
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2018-04-26 20:35:34 -0300

    use close  instead of sleep

--- hub_example_test.go
+++ hub_example_test.go
@@ -2,39 +2,43 @@ package hub_test
 
 import (
 	"fmt"
-	"time"
+	"sync"
 
 	"github.com/leandro-lugaresi/hub"
 )
 
 func ExampleHub() {
 	h := hub.New()
-
+	var wg sync.WaitGroup
 	// the cap param is used to create one buffered channel with cap = 10
 	// If you wan an unbuferred channel use the 0 cap
 	sub := h.Subscribe("account.*.failed", 10)
+	wg.Add(1)
 	go func(s hub.Subscription) {
 		for msg := range s.Receiver {
 			fmt.Printf("receive msg with topic %s and id %d\n", msg.Name, msg.Fields["id"])
 		}
+		wg.Done()
 	}(sub)
 
 	h.Publish(hub.Message{
 		Name:   "account.login.failed",
 		Fields: hub.Fields{"id": 123},
 	})
-
 	h.Publish(hub.Message{
 		Name:   "account.changepassword.failed",
 		Fields: hub.Fields{"id": 456},
 	})
-
 	h.Publish(hub.Message{
 		Name:   "account.foo.failed",
 		Fields: hub.Fields{"id": 789},
 	})
-	// TODO: remove sleep when the close method is implemented
-	time.Sleep(time.Second)
+
+	// finish all the subscribers
+	h.Close()
+	// wait until finish all the messages on buffer
+	wg.Wait()
+
 	// Output:
 	// receive msg with topic account.login.failed and id 123
 	// receive msg with topic account.changepassword.failed and id 456
