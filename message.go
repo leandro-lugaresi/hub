15520d09a4cbbc98779f4539f20212cf6bfe292f
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2020-07-24 17:37:16 -0300

    Added String representation for Fields

--- message.go
+++ message.go
@@ -1,5 +1,11 @@
 package hub
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 type (
 	// Fields is a [key]value storage for Messages values.
 	Fields map[string]interface{}
@@ -16,4 +22,19 @@ type (
 // Topic return the message topic used when the message was sended.
 func (m *Message) Topic() string {
 	return m.Name
+}
+
+func (f Fields) String() string {
+	if len(f) == 0 {
+		return "Fields(<empty>)"
+	}
+
+	fields := make([]string, 0, len(f))
+	for k, v := range f {
+		fields = append(fields, fmt.Sprintf("[%s]%v", k, v))
+	}
+
+	sort.Strings(fields)
+
+	return "Fields( " + strings.Join(fields, ", ") + " )"
 }
